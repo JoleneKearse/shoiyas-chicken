1c4895a51c5fa475cdbff2fb3efb751321ea510f
JoleneKearse <pharaohnutz@gmail.com>
2024-02-13 15:29:12 -0500

    chore: adjust text in Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,7 @@
 const Intro = () => {
   return (
     <section className="py-20 px-6 bg-brand-800">
-      <p className="text-stone-200 font-sans-serif text-xl">Looking for delicious and healthy eating in Sarnia. We are Completely Gluten-Free and Halal. Our meal options are Oven Baked and Never deep fried.</p>
+      <p className="text-xl text-brand-100">Looking for delicious and healthy eating in Sarnia. We are Completely Gluten-Free and Halal. Our meal options are Oven Baked and Never deep fried.</p>
     </section>
   )
 }
