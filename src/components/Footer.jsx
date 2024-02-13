6b396d30dac9498cb036256e100280ff87de6cc9
JoleneKearse <pharaohnutz@gmail.com>
2024-02-13 15:56:57 -0500

    chore: adjust footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,39 +6,39 @@ const Footer = () => {
   return (
     <footer className="bg-brand-blue sticky bottom-0 p-6">
       <div className="flex justify-between items-center">
-        <span className="block text-4xl text-brand-300 font-courgette font-black bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent pb-3">Shoiya's</span>
-        <ul className="flex flex-col gap-3 font-roboto">
-          <li>
+        <span className="block text-2xl text-brand-300 font-courgette font-black bg-gradient-to-r from-cyan-500 to-blue-500 bg-clip-text text-transparent pb-3">Shoiya's</span>
+        <ul className="flex gap-5 font-roboto">
+          <li className="hover:text-brand-300">
             <a href="https://www.facebook.com/profile.php?id=100093559993326" target="_blank" rel="noopener noreferrer">
               <div className="flex gap-2">
                 <img
                   src={Facebook}
                   alt="Facebook link"
-                  className="w-5 hover:backdrop-saturate-100 hover:w-6"
+                  className="w-5 hover:backdrop-saturate-100"
                 />
                 <p>Facebook</p>
               </div>
             </a>
           </li>
-          <li>
+          <li className="hover:text-brand-300">
             <a href="https://www.google.com/maps/place/537+Christina+St+N,+Sarnia,+ON+N7T+5W5/@42.9840469,-82.403669,17z/data=!3m1!4b1!4m6!3m5!1s0x88259cb9bea27027:0x6296d75c5652ac15!8m2!3d42.984043!4d-82.4010941!16s%2Fg%2F11c2gnvs3s?entry=ttu" target="_blank" rel="noopener noreferrer">
               <div className="flex gap-2">
                 <img
                   src={Map}
                   alt="directions"
-                  className="w-5 hover:backdrop-saturate-100 hover:w-6"
+                  className="w-5 hover:backdrop-saturate-100"
                 />
                 <p>Get directions</p>
               </div>
             </a>
           </li>
-          <li>
+          <li className="hover:text-brand-300">
             <a href="/menu" target="_blank" rel="noopener noreferrer">
               <div className="flex gap-2">
                 <img
                   src={Food}
                   alt="menu"
-                  className="w-5 hover:backdrop-saturate-100 hover:w-6"
+                  className="w-5 hover:backdrop-saturate-100"
                 />
                 <p>View our menu</p>
               </div>
