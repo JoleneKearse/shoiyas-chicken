2e36b88378b8f4062373a04591dd2565bc9ba17e
JoleneKearse <pharaohnutz@gmail.com>
2024-02-13 15:45:31 -0500

    chore: address text accessibility

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,8 @@
 const Intro = () => {
   return (
     <section className="py-20 px-6 bg-brand-800">
-      <p className="text-xl text-brand-100">Looking for delicious and healthy eating in Sarnia. We are Completely Gluten-Free and Halal. Our meal options are Oven Baked and Never deep fried.</p>
+      <p className="text-xl text-brand-100 leading-10 max-w-prose mx-auto">Looking for <span className="text-brand-300">delicious</span> and <span className="text-brand-300">healthy</span> eating in Sarnia?</p> 
+      <p className="text-xl text-brand-100 leading-10 max-w-prose mx-auto">We are completely <span className="text-brand-300">Gluten-Free</span> and <span className="text-brand-300">Halal</span>. Our meal options are <span className="text-brand-300">oven baked</span> and never deep fried.</p>
     </section>
   )
 }
