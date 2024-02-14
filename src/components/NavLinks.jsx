f702b007895afc32b7b4cc53db7f2bbb07e9a494
JoleneKearse <pharaohnutz@gmail.com>
2024-02-14 13:53:21 -0500

    feat: update to Link for refresh-free internal navigation

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -6,14 +6,12 @@ const NavLinks = (props) => {
     <ul className="text-brand-100 font-roboto flex gap-6 flex-col items-center lg:flex-row lg:gap-8">
       {props.navLinkPaths.map((link, index) => (
         <li key={index}>
-          <a
-            href={link.path}
-            target="_blank"
-            rel="noopener noreferrer"
+          <Link
+            to={link.path}
             className="pb-6 text-brand-blue font-semibold text-lg hover:underline hover:underline-offset-8 hover:decoration-wavy hover:text-brand-300 lg:text-brand-300"
           >
             {link.name}
-          </a>
+          </Link>
         </li>
       ))}
     </ul>
