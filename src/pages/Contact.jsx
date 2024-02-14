744b79d2778a56d157c455c957e8341db76c7d25
JoleneKearse <pharaohnutz@gmail.com>
2024-02-14 15:26:13 -0500

    chore: add overscroll none to pages

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ import Header from "../components/Header"
 
 const Contact = () => {
   return (
-    <div className="static h-full absolute w-full bg-brand-blue">
+    <div className="static h-full absolute w-full bg-brand-blue overscroll-none">
       <Header />
       <main className="py-20 px-6 bg-brand-800 lg:px-96">
         <ContactDetails />
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer"
 
 const HomePage = () => {
   return (
-    <div className="static h-full absolute w-full bg-brand-blue">
+    <div className="static h-full absolute w-full bg-brand-blue overscroll-none">
       <Header />
       <main className="py-20 px-6 bg-brand-800">
         <Intro />
