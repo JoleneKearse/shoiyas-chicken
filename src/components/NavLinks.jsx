86ee2085a240313fd95cda1a5776f2b5c714b242
JoleneKearse <pharaohnutz@gmail.com>
2024-02-14 16:05:16 -0500

    feat: add visual cues showing user which page they're on and disabled pointer

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,17 +1,20 @@
 /* eslint-disable react/prop-types */
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const NavLinks = (props) => {
   return (
-    <ul className="text-brand-100 font-roboto flex gap-6 flex-col items-center lg:flex-row lg:gap-8">
+    <ul className="font-roboto flex gap-6 flex-col items-center lg:flex-row lg:gap-8">
       {props.navLinkPaths.map((link, index) => (
         <li key={index}>
-          <Link
+          <NavLink
             to={link.path}
-            className="pb-6 text-brand-blue font-semibold text-lg hover:underline hover:underline-offset-8 hover:decoration-wavy hover:text-brand-300 lg:text-brand-300"
+            className={({ isActive }) =>
+              `pb-6 text-lg lg:text-brand-300
+            ${isActive ? "text-brand-800 font-medium pointer-events-none lg:text-brand-800" : "text-brand-100 hover:underline hover:underline-offset-8 hover:decoration-wavy hover:text-brand-300"}`
+            }
           >
             {link.name}
-          </Link>
+          </NavLink>
         </li>
       ))}
     </ul>
