0baddd3cf2f0873d44632af0a993ed440a562f1c
JoleneKearse <pharaohnutz@gmail.com>
2024-02-14 15:27:23 -0500

    chore: rename ContactPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
-import Contact from "./pages/Contact"
+import ContactPage from "./pages/ContactPage"
 
 function App() {
 
@@ -8,7 +8,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Router>
   )
